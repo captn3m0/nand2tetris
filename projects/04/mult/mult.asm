37c0f41a4a50dc79615baa2341b7631691f2db8a
Nemo <me@captnemo.in>
2020-05-27 19:44:30 +0530

    [04/mult] Multiply is possible

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,38 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+@R2
+M=0
+
 // Put your code here.
+(LOOP)
+
+@R0
+D=M
+// Jump to end if R0 = 0
+@END
+D;JEQ
+
+@R2
+D=M
+
+@R1
+// Increase D by R1
+D=D+M
+
+// Save final value back to result
+
+@R2
+M=D
+
+// R0=R0-1
+@R0
+M=M-1
+
+// Otherwise, go to loop
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
